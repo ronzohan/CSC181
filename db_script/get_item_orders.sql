6dbfa10203d1703313806b2d89a153f9595ab473
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-08-18 19:49:47 +0800

    Merge getter for item orders

--- db_script/get_item_orders.sql
+++ db_script/get_item_orders.sql
@@ -1,6 +1,7 @@
  create table orders(
-    order_id int primary key,
-    menu_name text
+    order_id serial primary key,
+    menu_name text,
+    order_time timestamp
 );
 
 --create language plpgsql;
@@ -15,8 +16,19 @@ $$
 $$
  language 'sql';
 -- HOW TO USE:
--- select * from get_team_per_name('Chelsea');
+-- select * from get_items_ordered(1);
 
 
-
+--create language plpgsql;
+--controller
+--view
+create or replace function 
+    get_items_ordered_all(out int, out text,out timestamp) 
+returns setof record as
+$$ 
+     select order_id, menu_name,order_time from orders    
+$$
+ language 'sql';
+-- HOW TO USE:
+-- select * from get_items_ordered_all();
 
